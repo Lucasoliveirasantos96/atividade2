c0452bcf37d6904cbd747f478258b81130741376
arqdsis <arqdsis@mocli10208.animaedu.intranet>
2017-09-01 21:19:30 -0300

    Initial commit

---
+++ app/src/main/java/br/lucas/atividade2/MainActivity.java
@@ -0,0 +1,17 @@
+package br.lucas.atividade2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "br.usjt.meuprimeiroapp.MESSAGE";
+    //será chamado quando o usuário clicar em Enviar public void sendMessage(View view){
+    Intent intent = new Intent(this, DisplayMessageActivity.class);
+    EditText editText = (EditText) findViewById(R.id.edit_message);
+    String message = editText.getText().toString();
+    intent.putExtra(EXTRA_MESSAGE, message);
+    startActivity(intent);
+}
+}
